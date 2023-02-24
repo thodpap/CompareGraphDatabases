00b03d7af76e25aa15adbc6869b5f994458a3ea9
Thodoris <paparrigopoulosthodoris@gmail.com>
2023-02-24 17:41:58 +0200

    delete

--- main_code/helper/delete_data/delete_data.py
+++ main_code/helper/delete_data/delete_data.py
@@ -83,3 +83,183 @@ def delete_data(hg, NUMBER_OF_vertices):
         "vertices":vertices
     }
        
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+def check_task_status(graph_name, task_id):
+    import requests
+
+    url = f"http://localhost:8081/graphs/{graph_name}/tasks/{task_id}"
+    response = requests.get(url=url)
+    return eval(response.content)["task_status"]
+
+def count_edges(graph_name="node_10"):
+    import requests
+
+    url_ = f"http://localhost:8081/gremlin"
+    query_ = graph_name + f'.traversal().E().count()'
+
+    response = requests.get(url_ + "?gremlin=" + query_)
+    return eval(response.content)["result"]["data"][0]
+
+# def delete_edges_vertex_gremlin_sync(graph_name="node_10", vertex=1):
+#     import requests
+#     url_ = f"http://localhost:8081/gremlin"
+#     query_ = graph_name + f'.traversal().V("1:{vertex}")'
+
+#     response = requests.delete(url_ + "?gremlin=" + query_)
+    
+#     return eval(response.content)["result"]["data"]
+
+
+#########################################################
+# Drop edges and vertices
+
+def delete_all_edges_gremlin_sync(graph_name="node_10"):
+    import requests
+
+    url_ = f"http://localhost:8081/gremlin"
+    query_ = graph_name + f'.traversal().E().drop()'
+
+    response = requests.delete(url_ + "?gremlin=" + query_)
+    return eval(response.content)
+
+def delete_all_vertices_gremlin_sync(graph_name="node_10"):
+    import requests
+
+    url_ = f"http://localhost:8081/gremlin"
+    query_ = graph_name + f'.traversal().V().drop()'
+
+    response = requests.delete(url_ + "?gremlin=" + query_)
+    return eval(response.content)
+
+def delete_edges_gremlin_alltogether(graph_name="node_10"):
+    import time
+
+    edges_count = count_edges()
+
+    start_time = time.time()
+    
+    delete_all_edges_gremlin_sync(graph_name)
+    
+    total = time.time() - start_time
+
+    return {
+        "mean_time": total / edges_count, 
+        "total_time": total
+    }
+
+def delete_vertices_gremlin_alltogether(graph_name="node_10", Nodes=10):
+    import time
+
+    start_time = time.time()
+    delete_all_vertices_gremlin_sync(graph_name)
+    total = time.time() - start_time
+
+    return {
+        "mean_time": total / Nodes, 
+        "total_time": total
+    }
+
+def delete_gremlin_alltogether(graph_name="node_10", Nodes=10):
+    return {
+        "edges": delete_edges_gremlin_alltogether(graph_name),
+        "vertices": delete_vertices_gremlin_alltogether(graph_name, Nodes)
+    }
+
+
+# -------------------------------------------------------------------------------------
+
+
+def read_edges_of_vertex_gremlin(graph_name="node_10", vertex=1):
+    import requests
+    
+    url_ = f"http://localhost:8081/gremlin"
+    query_ = graph_name + f'.traversal().V("1:{vertex}").outE().count()'
+
+    response = requests.get(url_ + "?gremlin=" + query_)
+    return eval(response.content)["result"]["data"][0]
+
+def delete_edges_by_vertex_gremlin(graph_name="node_10", vertex=1):
+    import requests
+    
+    nodes_count = read_edges_of_vertex_gremlin(graph_name, vertex)
+    
+    url_ = f"http://localhost:8081/gremlin"
+    query_ = graph_name + f'.traversal().V("1:{vertex}").outE().drop()'
+
+    response = requests.delete(url_ + "?gremlin=" + query_)
+    return eval(response.content), nodes_count
+
+def delete_vertices_by_vertex_gremlin(graph_name="node_10", vertex=1):
+    import requests
+    
+    url_ = f"http://localhost:8081/gremlin"
+    query_ = graph_name + f'.traversal().V("1:{vertex}").drop()'
+
+    response = requests.delete(url_ + "?gremlin=" + query_)
+    return eval(response.content)
+
+def delete_edges_gremlin_one_by_one(graph_name="node_10", Nodes=10):
+    import time
+
+    min_ = 1e1000
+    max_ = 0
+    mean_ = 0
+
+    start_time = time.time()
+    
+    for vertex in range(1, Nodes + 1):
+        begin = time.time()
+        
+        response, edge_count = delete_edges_by_vertex_gremlin(graph_name, vertex)
+        
+        end = time.time() - begin
+
+        min_ = min(min_, end / edge_count)
+        max_ = max(max_, end / edge_count)
+        mean_ += end / edge_count
+
+    total = time.time() - start_time
+
+    return {
+        "min": min_,
+        "mean": mean_, 
+        "max": max_, 
+        "total_time": total
+    }    
+
+
+def delete_vertices_gremlin_one_by_one(graph_name="node_10", Nodes=10):
+    import time
+
+    min_ = 1e1000
+    max_ = 0
+    mean_ = 0
+
+    start_time = time.time()
+    
+    for vertex in range(1, Nodes + 1):
+        begin = time.time()
+        
+        response = delete_vertices_by_vertex_gremlin(graph_name, vertex)
+        
+        end = time.time() - begin
+
+        min_ = min(min_, end)
+        max_ = max(max_, end)
+        mean_ += end
+
+    total = time.time() - start_time
+
+    return {
+        "min": min_,
+        "mean": mean_ / Nodes, 
+        "max": max_, 
+        "total_time": total
+    }    
+
+def delete_all_one_by_one_gremlin(graph_name, vertices):
+    return {
+        "edges": delete_edges_gremlin_one_by_one(graph_name, vertices), 
+        "vertices": delete_vertices_gremlin_one_by_one(graph_name, vertices)
+    }
--- main_code/test_delete.py
+++ main_code/test_delete.py
@@ -9,12 +9,38 @@ if __name__ == "__main__":
 
     # Add an argument
     parser.add_argument('graph_name', type=str, help='the graph name')
+    # parser.add_argument('method', type=int, help='method: all together - 1 or one by one - 2')
 
     # Parse the command-line arguments
     args = parser.parse_args()
 
     assert "node_1" in args.graph_name
+    
+    if args.graph_name == "node_10":
+        vertices = 10
+    elif args.graph_name == "node_100":
+        vertices = 100
+    elif args.graph_name == "node_1000":
+        vertices = 1000
+    elif args.graph_name == "node_10000":
+        vertices = 10000
+    elif args.graph_name == "node_100000":
+        vertices = 100000
+    elif args.graph_name == "node_1000000":
+        vertices = 1000000
+    else:
+        assert False
+
+    # if args.method == 1:
+    #     allTogether = True
+    # else:
+    #     allTogether = False
 
     hg = PyHugeGraphClient.HugeGraphClient("http://localhost", "8081", args.graph_name)
 
-    print(delete_data.delete_data(hg, 10))
+    # print(delete_data.delete_data(hg, 10))
+
+    print(delete_data.delete_gremlin_alltogether(args.graph_name, vertices))
+
+    print(delete_data.delete_all_one_by_one_gremlin(args.graph_name, vertices))
+    
