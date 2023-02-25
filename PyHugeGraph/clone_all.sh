f0efd9364a9b9c318786f8441867ebe4db4e4d77
Thodoris <paparrigopoulosthodoris@gmail.com>
2023-02-25 18:31:48 +0200

    update clone all

--- PyHugeGraph/clone_all.sh
+++ PyHugeGraph/clone_all.sh
@@ -1,4 +1,7 @@
 curl -X POST http://localhost:8081/graphs/node_10?clone_graph_name=hugegraph
 curl -X POST http://localhost:8081/graphs/node_100?clone_graph_name=hugegraph
 curl -X POST http://localhost:8081/graphs/node_1000?clone_graph_name=hugegraph
+curl -X POST http://localhost:8081/graphs/node_10000?clone_graph_name=hugegraph
+curl -X POST http://localhost:8081/graphs/node_100000?clone_graph_name=hugegraph
+curl -X POST http://localhost:8081/graphs/node_1000000?clone_graph_name=hugegraph
 curl -X POST http://localhost:8081/graphs/test?clone_graph_name=hugegraph
